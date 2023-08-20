1d2af1010b29f02cfd67a5d2accf20616e8c7353
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-08-20 10:09:52 -0300

    Init baseline for Spring Boot Restful APIs

---
+++ src/main/java/com/example/baselinespringapi/BaselineSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.baselinespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaselineSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaselineSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/baselinespringapi/BaselineSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baselinespringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaselineSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
